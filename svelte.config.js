0a61478f8ab01069140d7827d1ec7a04afa8635b
️Yaskur Dyas⚔⚔️⚔ <dyas@yaskur.com>
2025-06-21 02:00:48 +0700

    chore: add vitePreprocess to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
+	preprocess: vitePreprocess(),
 	kit: {
 			adapter: adapter({
 					// default options are shown. On some platforms
